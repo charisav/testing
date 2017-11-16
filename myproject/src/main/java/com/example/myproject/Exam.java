9e886db18241531c6283bd6659d1fd356dfb755d
Buru <charis_gm@hotmail.com>
2017-11-17 00:30:31 +0200

    Greeting functionality

--- myproject/src/main/java/com/example/myproject/Exam.java
+++ myproject/src/main/java/com/example/myproject/Exam.java
@@ -1,5 +1,7 @@
 package com.example.myproject;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @EnableAutoConfiguration
 public class Exam {
-
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+	
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "What's your name?";
 	}
 	
-	@RequestMapping("/addon")
-	String addon() {
-		return "How are you today?";
-	}
+	@RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Exam.class, args);
