2ab8349094dca79eabd6c8fea7124ac85319ecd6
Buru <charis_gm@hotmail.com>
2017-11-26 22:34:26 +0200

    added encryption password system

--- reg-login-2/src/main/java/com/example/demo/resource/UserController.java
+++ reg-login-2/src/main/java/com/example/demo/resource/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.resource;
 
 import com.example.demo.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,23 +21,25 @@ public class UserController {
 	@Autowired
 	private UserValidator userValidator;
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	@GetMapping("/register")
 	public String register(Model model) {
 		model.addAttribute("user", new User());
 
 		return "register";
 	}
-	
-	@PostMapping("/register")
-	public String register(@ModelAttribute("user") User user, BindingResult bindingResult,Model model) {
 
+	@PostMapping("/register")
+	public String register(@ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
 		userValidator.validate(user, bindingResult);
 
 		if (bindingResult.hasErrors()) {
 			return "register";
 		}
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
-
 
 		return "redirect:/home";
 	}
